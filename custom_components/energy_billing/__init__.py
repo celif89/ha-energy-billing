0a10dc903a0cc55a7be4960ef5de1bfa5bf02985
celif89 <a.filec@gmail.com>
2026-05-05 13:19:16 +0200

    Update __init__.py

--- custom_components/energy_billing/__init__.py
+++ custom_components/energy_billing/__init__.py
@@ -1,5 +1,8 @@
 async def async_setup_entry(hass, entry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Dodaliśmy literkę 's' w setups oraz nawiasy [] wokół "sensor"
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+async def async_unload_entry(hass, entry):
+    # To pozwoli na poprawne usunięcie integracji bez restartu
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
